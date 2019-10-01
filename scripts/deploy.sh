645e536cd175b7072b4125d49b110bcc4c78f0e9
LukyVj <lucas.bonomi@gmail.com>
2019-10-01 11:47:32 +0200

    Updae

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,1 @@
-yarn build && rm build/static/js/*.map && mv build docs && git add . && git commit -m"Updae" && git push origin master && surge ./build --domain https://backup-nazarfinder.surge.sh
+yarn build && rm build/static/js/*.map && mv build docs && git add . && git commit -m"Updae" && git push origin master && surge ./docs --domain https://backup-nazarfinder.surge.sh
