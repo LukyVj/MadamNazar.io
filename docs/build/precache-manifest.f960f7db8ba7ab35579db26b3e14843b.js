f4b570d7f74d5d3af6b5fad08034386158c5ac4d
LukyVj <lucas.bonomi@gmail.com>
2019-10-01 12:22:11 +0200

    Updae

---
+++ docs/build/precache-manifest.f960f7db8ba7ab35579db26b3e14843b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970b263e1dbff908f5e077a716c6c1cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e0edb2405cbbbb9992f",
+    "url": "/static/css/main.52e57225.chunk.css"
+  },
+  {
+    "revision": "bb4cea3e05b968056181",
+    "url": "/static/js/2.5378b18d.chunk.js"
+  },
+  {
+    "revision": "9e0edb2405cbbbb9992f",
+    "url": "/static/js/main.7b05dcf4.chunk.js"
+  },
+  {
+    "revision": "389ff49b08ffbd2d92fd",
+    "url": "/static/js/runtime~main.ec2b0575.js"
+  },
+  {
+    "revision": "b60b4517a18c866cb19522c6bb5117f9",
+    "url": "/static/media/RDRCatalogueBold-Bold.b60b4517.woff2"
+  },
+  {
+    "revision": "b3c9ab0ca4775dec5db189e67ede3d4d",
+    "url": "/static/media/RDRHapna-Regular.b3c9ab0c.woff2"
+  },
+  {
+    "revision": "9ad5260c09812aeb38a403d3498e7466",
+    "url": "/static/media/RDRLino-Regular.9ad5260c.woff2"
+  },
+  {
+    "revision": "4fb59aca65a6346cf6f314973b612510",
+    "url": "/static/media/bgMainSml.4fb59aca.jpg"
+  },
+  {
+    "revision": "c1e174395a2da24a71d39ef73133bec7",
+    "url": "/static/media/bgRip.c1e17439.png"
+  },
+  {
+    "revision": "ec832100ea20db384b4758abdcc18a01",
+    "url": "/static/media/hat.ec832100.png"
+  },
+  {
+    "revision": "06a266a7dbecfbc41dfb4fae20077572",
+    "url": "/static/media/nazarBg.06a266a7.png"
+  },
+  {
+    "revision": "f984bf8cf1a2848733c164b85647bcbc",
+    "url": "/static/media/sheet.f984bf8c.png"
+  },
+  {
+    "revision": "bdb43215d285cbc3149f00b301d8d332",
+    "url": "/static/media/star.bdb43215.svg"
+  }
+]);
