11aa7e4e97c2ccdbe36d2fe3c333163954c35568
LukyVj <lucas.bonomi@gmail.com>
2019-11-06 23:17:27 +0100

    add poggers clone, for now

--- src/pages/CollectorMap.js
+++ src/pages/CollectorMap.js
@@ -84,7 +84,7 @@ class CollectorMap extends Component {
           </button>
 
           <Iframe
-            url={`${COLLECTOR_MAP_URL}`}
+            url={`http://localhost:8000/`}
             title="Jean Ropke RDR2 Collector Map"
             height={this.state.expandMap === false ? 700 : "100%"}
             frameBorder="border: 4px solid var(--Armadillo);"
