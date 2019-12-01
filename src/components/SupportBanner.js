c4874d9fcbe11629949c9dcca7071f469231627b
German Topalov <ru00161131@gmail.com>
2019-12-01 15:37:16 +0200

    refactor: * refactored App.js, SupportBanner and PatreonModal * optimized patreon cookie logic at App.js, created patreonAd control methods * new styling for PatreonModals, now it fits on all screens, added content scroll lock when modal is active, modal now closes on click outside

--- src/components/SupportBanner.js
+++ src/components/SupportBanner.js
@@ -1,13 +1,7 @@
 /** @jsx jsx */
-import React, { Component } from "react";
-import ReactGA from "react-ga";
+import React from "react";
 import { css, jsx } from "@emotion/core";
-
-import { docCookies } from "../scripts/cookies";
 import { WEBSITE_NAME } from "../scripts/constants";
-import { maxAgeToGMT } from "../scripts/helpers";
-import frame from "../images/frame.png";
-import { styles } from "ansi-colors";
 
 const bannerStyles = {
   root: css`
@@ -32,44 +26,35 @@ const bannerStyles = {
     `
 };
 
-export const SupportBanner = ({ parent }) => (
-  <div
-    className="pv-8 ph-16 fsz-12 md:fsz-14 bxs-default"
-    css={bannerStyles.root}
-  >
-    <div className="maw-600 pos-relative m-auto d-flex jc-between ta-center">
-      {" "}
-      <span
-        className="cursor-pointer fx-10"
-        css={css`
+export const SupportBanner = props => {
+  const { onOpen, onClose } = props;
+  return (
+    <div
+      className="pv-8 ph-16 fsz-12 md:fsz-14 bxs-default"
+      css={bannerStyles.root}
+    >
+      <div className="maw-600 pos-relative m-auto d-flex jc-between ta-center">
+        {" "}
+        <span
+          className="cursor-pointer fx-10"
+          css={css`
           text-decoration: none;
           color: white;
           font-weight: bold;
           text-shadow: 1px 1px 10px rgba(0, 0, 0, 0.8);
         `}
-        onClick={() => {
-          parent.setState({
-            showPatreonAbout: true,
-            showPatreonAd: false
-          });
-        }}
-      >
+          onClick={onOpen}
+        >
         <b>
           Support {WEBSITE_NAME}! <br />
           Click to learn more
         </b>
          
       </span>
-      <span className="as-center js-center fx-2">
+        <span className="as-center js-center fx-2">
         <button
           className="app-none bdw-0 bgc-transparent p-0 m-0 va-middle cursor-pointer"
-          onClick={() => {
-            parent.setState({
-              showPatreonAd: false,
-              showPatreonAbout: false
-            });
-            docCookies.setItem("patreon-ad", "false", maxAgeToGMT(999));
-          }}
+          onClick={onClose}
         >
           <img
             src={require("../images/cancel-icon.svg")}
@@ -78,6 +63,7 @@ export const SupportBanner = ({ parent }) => (
           />
         </button>
       </span>
+      </div>
     </div>
-  </div>
-);
+  )
+};
