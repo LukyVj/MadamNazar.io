19a0883ba25214a789d6c3d643c66dc5f67c838c
lukyvj <lucas.bonomi@gmail.com>
2020-02-03 14:34:45 +0100

    Use proper cycles data

--- public/CollectorsMap/assets/js/cycles.js
+++ public/CollectorsMap/assets/js/cycles.js
@@ -1,18 +1,20 @@
 var Cycles = {
-    data: [],
-    load: function () {
-        $.getJSON('data/cycles.json?nocache=' + nocache)
-            .done(function (_data) {
-                Cycles.data = _data;
-                Cycles.setCycles();
+  data: [],
+  load: function() {
+    $.getJSON(
+      "https://jeanropke.github.io/RDR2CollectorsMap/data/cycles.json?nocache=" +
+        nocache
+    ).done(function(_data) {
+      Cycles.data = _data;
+      Cycles.setCycles();
 
-                $('.cycle-data').text(Cycles.data.updated_at);
-            });
-        console.log('cycles loaded');
-    },
-    setCycles: function () {
-        $.each(Cycles.data.cycles[Cycles.data.current], function (key, value) {
-            $(`input[name=${key}]`).val(value);
-        });
-    }
-}
+      $(".cycle-data").text(Cycles.data.updated_at);
+    });
+    console.log("cycles loaded");
+  },
+  setCycles: function() {
+    $.each(Cycles.data.cycles[Cycles.data.current], function(key, value) {
+      $(`input[name=${key}]`).val(value);
+    });
+  }
+};
