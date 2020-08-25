531753bf38ce4110eb4983bed50ac67bf0fa28df
lukyvj <lucas.bonomi@gmail.com>
2020-08-25 15:08:52 +0200

    oops discord url

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -2,7 +2,7 @@ export const WEBSITE_NAME = "MadamNazar.io";
 export const TWITTER_URL = "https://twitter.com/MadamNazarIO";
 export const TWITTER_NAME = "@MadamNazarIO";
 export const PATREON_URL = "https://support-madamnazario.surge.sh/";
-export const DISCORD_URL = "https://discord.gg/MhMPPnw";
+export const DISCORD_URL = "https://discord.gg/2WZaTj9";
 
 export const DEV_API =
   "https://madam-nazar-location-api-2.herokuapp.com/v2/today";
